ab9aa0ec358dee0febdd773488af1f167942238a
cromeo98 <romeo.christian98@gmail.com>
2021-07-08 18:14:22 +0200

    end header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="lead">
-    ninos
-  </div>
+  <header class="ps-3">
+
+    <img id="logo" src="../assets/spotify-logo.svg" alt="">
+
+  </header>
 </template>
 
 <script>
@@ -12,4 +14,16 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+
+header{
+  height: 60px;
+  background-color: rgb(46, 58, 70);
+  display: flex;
+  align-items: center;
+}
+
+#logo{
+  width: 45px;
+}
+
 </style>
